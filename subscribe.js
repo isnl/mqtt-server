b73cf9392e15388ecfc2e9303761f81d15d4a51b
Peanut <35526117+isnl@users.noreply.github.com>
2019-12-04 21:36:53 +0800

    Update subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -1,20 +1,13 @@
-var mqtt = require('mqtt');
-// var mqtt = require('./node_modules/mqtt/dist/mqtt.min.js')
-var client = mqtt.connect("mqtt://127.0.0.1:1883");   //指定服务端地址和端口
+const mqtt = require("mqtt");
+// const mqtt = require('./node_modules/mqtt/dist/mqtt.min.js')
+const client = mqtt.connect("mqtt://127.0.0.1:1883"); //指定服务端地址和端口
 
-client.on('connect', function () {
-    console.log("服务器连接成功");
-
-    // connected = client.connected
-
-    client.subscribe('location', { qos: 1 });//订阅主题为test的消息  
-})
-
-
-
-client.on('message', function (top, message) {
-    console.log('当前topic', top);
-
-    console.log(message.toString());
+client.on("connect", function() {
+  console.log("服务器连接成功");
+  // connected = client.connected
+  client.subscribe("temperature", { qos: 1 }); //订阅主题为test的消息
 });
-
+client.on("message", function(top, message) {
+  console.log("当前topic：", top);
+  console.log('当前温度：',message.toString());
+});
