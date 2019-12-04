80fd5b4a63f6359043b9cf4b8913252d44c4b4b7
Peanut <35526117+isnl@users.noreply.github.com>
2019-12-04 21:36:18 +0800

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,32 +1,30 @@
-var mosca = require('mosca');
-//构建自带服务器
-var MqttServer = new mosca.Server({
-    port: 1883
+const mosca = require("mosca");
+const MqttServer = new mosca.Server({
+  port: 1883
 });
-//对服务器端口进行配置， 在此端口进行监听
-MqttServer.on('clientConnected', function(client) {
-    //监听连接
-    console.log('client connected', client.id);
+MqttServer.on("clientConnected", function (client) {
+  //当有客户端连接时的回调.
+  console.log("client connected", client.id);
 });
 /**
  * 监听MQTT主题消息
+ * 当客户端有连接发布主题消息时
  **/
-MqttServer.on('published', function(packet, client) {
-    //当客户端有连接发布主题消息
-    var topic = packet.topic;
-    switch (topic) {
-        case 'location':
-            // console.log('message-publish', packet.payload.toString());
-            //MQTT转发主题消息
-            //MqttServer.publish({ topic: 'other', payload: 'sssss' });
-            break;
-        case 'other':
-            console.log('message-123', packet.payload.toString());
-            break;
-    }
+MqttServer.on("published", function (packet, client) {
+  var topic = packet.topic;
+  switch (topic) {
+    case "temperature":
+      // console.log('message-publish', packet.payload.toString());
+      //MQTT可以转发主题消息至其他主题
+      //MqttServer.publish({ topic: 'other', payload: 'sssss' });
+      break;
+    case "other":
+      console.log("message-123", packet.payload.toString());
+      break;
+  }
 });
 
-MqttServer.on('ready', function() {
-    //当服务开启时
-    console.log('mqtt is running...');
+MqttServer.on("ready", function () {
+  //当服务开启时的回调
+  console.log("mqtt is running...");
 });
