8f434ec923bd4db408e2e9359422fc2bd9864dc3
Peanut <35526117+isnl@users.noreply.github.com>
2019-12-04 21:36:37 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,29 +1,7 @@
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://127.0.0.1:1883'); //连接到服务端
-const data = require("./points.js");
-
-function getPoints() {
-  const dataLen = data.length;
-  let points = []
-  for (let i = 0; i < 10; i++) {
-    const randomPoint = data[parseInt(Math.random() * dataLen - 1)];
-    const point = {
-      "id": i,
-      "verticalAccuracy": 10,
-      "speed": -1,
-      "longitude": randomPoint.lng,
-      "horizontalAccuracy": 65,
-      "provider": "gps",
-      "latitude": randomPoint.lat,
-      "accuracy": 65,
-      "direction": -1,
-      "altitude": 409.93402099609375
-    }
-    points.push(point);
-  }
-  return points;
-}
-setInterval(function () {
-  const points = getPoints()
-  client.publish('location', JSON.stringify(points), { qos: 0, retain: true });
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://127.0.0.1:1883"); //连接到mqtt服务端
+//写个定时器定时每隔3秒定时推送天气信息，此业务可替换为自己的实际需求
+setInterval(function() {
+  const value = Math.ceil(Math.random() * 40);
+  client.publish("temperature", value.toString(), { qos: 0, retain: true });
 }, 3000);
